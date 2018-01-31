6e96eafcf3ceb79a3e4d4fe84f18edc79d3cc941
shaghz <sh.khangaldy@gmail.com>
2018-01-31 17:55:29 +0330

    updated main.js

---
+++ components/List.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {hi} from './main.css';
+class List extends Component {
+    render() {
+        return (
+
+            <ul>
+                <li>1</li>
+                <li>2</li>
+            </ul>
+
+        );
+    }
+}
+
+export default List;
--- components/Main.js
+++ components/Main.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import {hi} from './main.css';
+import List from "../components/List"
 class Main extends Component {
+    // let YOUTUBE_API_KEY = AIzaSyAnzwEsXfyn5-WssI_unXARgPYY8CDdSII;
+    handleChange(event) {
+
+        console.log(this);
+    }
     render() {
         return (
-
+                <div>
                 <div className={hi}>
-                        <input placeholder="What to search in youtube?">
+                        <input  onKeyUp={this.handleChange.bind(this)} placeholder="What to search in youtube?">
                         </input>
-                        <button ><i class="fa fa-search" aria-hidden="true"></i></button>
+                        <button ><i className="fa fa-search" aria-hidden="true"></i></button>
 
                 </div>
-
+                <List />
+                </div>
         );
     }
 }
